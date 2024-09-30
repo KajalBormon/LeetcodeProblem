d266011ae37d6fea08c3f912efc7d6d818c7b839
KajalBormon <kajalbormon40@gmail.com>
2024-09-30 16:35:02 +0600

    Valid Parenthesis

--- 20_Valid_Parenthesis.js
+++ 20_Valid_Parenthesis.js
@@ -13,9 +13,9 @@ const stack = [];
 for(let i=0; i<s.length; i++){
     if(mapping.has(s[i])){
         stack.push(mapping.get(s[i]));
-        console.log(stack);
+     
     }else if(stack.pop() !== s[i]){
-        console.log( false);
+        return false;
     }
 }
-console.log(stack.length === 0);
+return stack.length === 0;
