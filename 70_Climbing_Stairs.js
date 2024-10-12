09b658bc9f76a9f9ba190829f1618b306010d8f8
KajalBormon <kajalbormon40@gmail.com>
2024-10-12 10:37:06 +0600

    Climbing stairs

--- 70_Climbing_Stairs.js
+++ 70_Climbing_Stairs.js
@@ -1,4 +1,4 @@
-/* --------Leetcode Solution------ */
+/* --------Leet code Solution------ */
 var climbStairs = function (n) {
     let dp = [];
     dp[1] = 1;
@@ -10,18 +10,3 @@ var climbStairs = function (n) {
 
     return dp[n];
 };
-
-
-/* ---------------Using PHP---------- */
-
-/* class Solution {
-    function climbStairs($n) {
-        $dp = [];
-        $dp[1] = 1;
-        $dp[2] = 2;
-        for($i=3; $i<=$n; $i++){
-            $dp[$i] = $dp[$i-1] + $dp[$i-2];
-        }
-        return $dp[$n];
-    }
-} */
