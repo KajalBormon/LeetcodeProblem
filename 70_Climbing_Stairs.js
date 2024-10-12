40cd76bc5cdcd020354121b9126a965b7fa3096e
KajalBormon <kajalbormon40@gmail.com>
2024-10-12 10:33:33 +0600

    Climbing stairs

---
+++ 70_Climbing_Stairs.js
@@ -0,0 +1,27 @@
+/* --------Leetcode Solution------ */
+var climbStairs = function (n) {
+    let dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(let i=3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+};
+
+
+/* ---------------Using PHP---------- */
+
+/* class Solution {
+    function climbStairs($n) {
+        $dp = [];
+        $dp[1] = 1;
+        $dp[2] = 2;
+        for($i=3; $i<=$n; $i++){
+            $dp[$i] = $dp[$i-1] + $dp[$i-2];
+        }
+        return $dp[$n];
+    }
+} */
